0936906297befc54fc50ca888a09a0c8e08cdcb1
Michiel Dijkstra <msdijkstra@gmail.com>
2017-10-29 15:34:28 +0100

    flaskblog

---
+++ sql.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+#create a new database
+with sqlite3.connect("blog.db") as connection:
+
+    #create cursor object
+    c = connection.cursor()
+
+    #create the table
+    c.execute("""CREATE TABLE posts
+        (title TEXT, post TEXT)
+        """)
+
+    #insert dummy data
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
